da0e6862de1692aa2e75cea23d35fcfb2e921ca8
Ricky <n01094968@gmail.com>
2019-10-01 14:15:26 -0400

    fixed

--- app/src/main/java/com/example/inclass1project/MainActivity.java
+++ app/src/main/java/com/example/inclass1project/MainActivity.java
@@ -25,39 +25,24 @@ public class MainActivity extends AppCompatActivity {
     //How to do Toast Button Part 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_transparent); //  here
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);// This is where the screen gets its design from the XML file. The change of theme cannot happen after this
+        // line. It has to be before it.
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        setTheme(R.style.AppTheme_LauncherAfterSplash);
-                    }
-                });
-            }
-        }).start();
 
         //mTextMessage = findViewById(R.id.message);
         //navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-//        myButton = (Button)findViewById(R.id.button);
+        myButton = (Button)findViewById(R.id.button);
 //
-//        myButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_LONG).show();
-//            }
-//        });
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_LONG).show();
+            }
+        });
 
 
 
