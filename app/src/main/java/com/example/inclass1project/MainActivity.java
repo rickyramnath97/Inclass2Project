7a74805eb39866e79893e4a616ec3c562d6c168a
Ricky <n01094968@gmail.com>
2019-10-01 12:27:04 -0400

    Has to redo lab 1 and aded splash screen

--- app/src/main/java/com/example/inclass1project/MainActivity.java
+++ app/src/main/java/com/example/inclass1project/MainActivity.java
@@ -1,42 +1,48 @@
 package com.example.inclass1project;
 
+
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ceng319.launcher.R;
+
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    //How to do Toast Button Part 1
+    protected Button myButton;
+    String myString = "Hello this is my first android APP";
 
+
+    //How to do Toast Button Part 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTheme(R.style.AppTheme);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        //mTextMessage = findViewById(R.id.message);
+        //navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-}
+        myButton = (Button)findViewById(R.id.button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+        }
+
+
+
+    }
